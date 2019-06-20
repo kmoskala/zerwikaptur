06682f04c2c92bd34f050b6f4c91f476b4954934
Katarzyna Moskała <kasia.moskala.20.03@gmail.com>
2019-06-20 15:58:14 +0200

    RWD, cookies. gallery, form

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+	
 	$('.header-class').height($(window).height());
 	
 	$('#header .navbar-menu').click(function () {
@@ -41,27 +42,89 @@ $(document).ready(function () {
 			$(this).css("transform", "rotate(45deg)");
 		}
 	});
-	//jssor_1_slider_init();
-
-	/*$('.trainers-container.owl-carousel').owlCarousel({
+	$('.trainers-container.owl-carousel').owlCarousel({
 		loop:true,
+		
+		autoplay: true,
+		autoplayTimeout:2000,
+		autoplayHoverPause: true,
 		margin:25,
 		nav:true,
-		items:2
-
+		responsive : {
+			// breakpoint from 0 up
+			0 : {
+				items: 1
+			},
+			768 : {
+				items: 2
+			},
+			991 : {
+				items:3
+			}
+			
+		}
+		
 	});
 	$('.slider.owl-carousel').owlCarousel({
 		loop:true,
 		margin:25,
 		nav:true,
-		items:3,
 		autoplay: true,
-		autoplayTimeout:100,
-		autoplayHoverPause: true
-	});*/
+		autoplayTimeout:2000,
+		autoplayHoverPause: true,
+		responsive : {
+			// breakpoint from 0 up
+			0 : {
+				items: 1
+			},
+			768 : {
+				items: 2
+			},
+			991 : {
+				items:3
+			}
+			
+		}
+	});
+	
+	
 	
 	
 });
+function WHCreateCookie(name, value, days) {
+	var date = new Date();
+	date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+	var expires = "; expires=" + date.toGMTString();
+	document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function WHReadCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+
+window.onload = WHCheckCookies;
+
+function WHCheckCookies() {
+	if (WHReadCookie('cookies_accepted') != 'T') {
+		var message_container = document.createElement('div');
+		message_container.id = 'cookies-message-container';
+		var html_code = '<div id="cookies-message" style="padding: 10px 0px; font-size: 14px; line-height: 22px; border-bottom: 1px solid #cda737; text-align: center; position: fixed; top: 0px; background-color: #000000; width: 100%; z-index: 999;">Ta strona używa ciasteczek (cookies), dzięki którym nasz serwis może działać lepiej. <a href="http://wszystkoociasteczkach.pl" target="_blank">Dowiedz się więcej</a><a href="javascript:WHCloseCookiesWindow();" id="accept-cookies-checkbox" name="accept-cookies" style="background-color: #cda737; padding: 5px 10px; color: #FFF; border-radius: 4px; -moz-border-radius: 4px; -webkit-border-radius: 4px; display: inline-block; margin-left: 10px; text-decoration: none; cursor: pointer;">Rozumiem</a></div>';
+		message_container.innerHTML = html_code;
+		document.body.appendChild(message_container);
+	}
+}
+
+function WHCloseCookiesWindow() {
+	WHCreateCookie('cookies_accepted', 'T', 365);
+	document.getElementById('cookies-message-container').removeChild(document.getElementById('cookies-message'));
+}
 
 // $(function(){
 // 	$("#header").load('components/header.html');
